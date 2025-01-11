de20f79e0958f32f5973266f277cb83028b6e0f3
Nerzula <alexzloid@gmail.com>
2025-01-11 23:19:52 +0300

    feat: resolve generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -314,8 +314,8 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((balance, [income, expense]) => balance + (income - expense), 0)
 }
 
 /**
@@ -330,8 +330,14 @@ function calculateBalance(/* arr */) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((acc, num, index) => {
+    if(index % chunkSize === 0) {
+      acc.push([]);
+    }
+    acc[acc.length - 1].push(num);
+    return acc;
+  }, []);
 }
 
 /**
@@ -346,8 +352,12 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = [];
+  for (let index = 0; index < len; index++) {
+    arr.push(2 * index + 1);
+  }
+  return arr;
 }
 
 /**
