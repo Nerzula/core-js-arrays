98678a503370e21f4a2fd0b5ad159f6376b654ed
Nerzula <alexzloid@gmail.com>
2025-01-11 17:59:42 +0300

    feat: resolve getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -125,10 +125,11 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if(!arr.length) return 0;
+  const res = arr.reduce((acc, num,) => acc + num , 0) / arr.length
+  return Number(res.toFixed(2));
 }
-
 /**
  * Checks if all strings in an array have the same length.
  *
