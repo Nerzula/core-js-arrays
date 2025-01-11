6f5e49c2f672ff793ec8d0a465f7deb408985846
Nerzula <alexzloid@gmail.com>
2025-01-12 00:30:42 +0300

    feat: resolve shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -523,9 +523,14 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const length = arr.length;
+  if (length === 0) return arr;
+
+  n = ((n % length) + length) % length;
+  return [...arr.slice(n), ...arr.slice(0, n)];
 }
+
 
 /**
  * Sorts digit names.
