01fef16282a30168b37699b8920e2384ecf092ac
Nerzula <alexzloid@gmail.com>
2025-01-11 14:38:20 +0300

    feat: resolve getIntervalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,14 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  let arr = [];
+
+  for (let index = start; index < end + 1; index++){
+    arr.push(index);
+  }
+
+  return arr;
 }
 
 /**
